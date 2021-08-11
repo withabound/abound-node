ec48171db2a3aca4d3bb8144732659b4b96fcc96
Mark Mullan <mark.mullan@clover.com>
2021-08-11 13:07:10 -0700

    WIP - expenses

---
+++ src/resources/Expenses.ts
@@ -0,0 +1,55 @@
+import { EmptyObject, Notes } from "./base/AboundResource";
+import { AboundBulkResponse, AboundResponse } from "./base/AboundResponse";
+import { AboundUserScopedResource } from "./base/AboundUserScopedResource";
+
+// request body
+export interface ExpenseRequest {
+  amount: number; // float
+  description: string;
+  date: string; // YYYY-MM-DD
+  expenseType?: ExpenseType;
+  taxCategory?: string;
+  foreignId?: string;
+  notes?: Notes;
+}
+
+export enum ExpenseType {
+  BUSINESS = "business",
+  PERSONAL = "personal",
+}
+
+// response body
+export interface Expense extends ExpenseRequest {
+  expenseId: Readonly<string>;
+  deductionAmount: number; // float
+  predictions: ExpensePredictions;
+}
+
+interface ExpensePredictions {
+  expenseTypePredictionScores: Record<string, number>; // 0 < values < 1
+  taxCategoryPredictionScores: Record<string, number>; // 0 < values < 1
+}
+
+export class Expenses extends AboundUserScopedResource<ExpenseRequest, Expense> {
+  path: string = "/expenses";
+
+  public async create(userId: string, expenses: ExpenseRequest[]): Promise<AboundBulkResponse<Expense>> {
+    return super.bulkCreateForUser(userId, { expenses });
+  }
+
+  public async list(userId: string): Promise<AboundBulkResponse<Expense>> {
+    return super.listForUser(userId);
+  }
+
+  public async retrieve(userId: string, expenseId: string): Promise<AboundResponse<Expense>> {
+    return super.retrieveForUser(userId, expenseId);
+  }
+
+  public async update(userId: string, expenseId: string, expense: Partial<ExpenseRequest>): Promise<AboundResponse<Expense>> {
+    return super.updateForUser(userId, expenseId, { expense });
+  }
+
+  public async delete(userId: string, expenseId: string): Promise<AboundResponse<EmptyObject>> {
+    return super.deleteForUser(userId, expenseId);
+  }
+}
