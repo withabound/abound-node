bcc6d18c55ceb1bd9c0a7ecb95eb3ba445b2c9a3
Mark Mullan <mark.mullan@clover.com>
2021-09-13 18:39:04 -0700

    REFACTOR: prefer hitting APIs in tests instead of stubbing responses (#25)
    * REFACTOR: prefer hitting APIs in tests instead of stubbing responses
    
    * string union for TaxPaymentDocumentType
    
    * add a TODO comment for the describe.skip test
    
    * use a real bank logo URL for documents.create

--- tests/resources/Mileages.spec.ts
+++ tests/resources/Mileages.spec.ts
@@ -1,61 +1,50 @@
-import nock from "nock";
-
 import Abound from "../../src/abound";
 import {
   AboundBulkResponse,
   AboundResponse,
 } from "../../src/resources/base/AboundResponse";
 import { Mileage } from "../../src/resources/Mileages";
-import { createAboundClient, randomDate, V2_SANDBOX_URL } from "../utils";
+import { createAboundClient, randomDate, TEST_USER_ID } from "../utils";
 
 describe("Abound Mileages", () => {
   let abound: Abound;
 
   beforeAll(() => {
     abound = createAboundClient();
-    initMocks();
-  });
-
-  afterAll(() => {
-    nock.restore();
   });
 
   describe("create", () => {
     it("returns a promise that resolves to a list of the bulk created Mileages on success", async () => {
-      const createdMileages: AboundBulkResponse<Mileage> =
-        await abound.mileages.create(
-          "userId_1dafc40ae9838c2c3e721b2c3c362252ab55eb9f",
-          [
-            {
-              distance: 21.1,
-              date: randomDate(),
-            },
-            {
-              distance: 89.1,
-              date: randomDate(),
-            },
-          ]
-        );
+      const randomDate1 = randomDate();
+      const randomDate2 = randomDate();
 
-      expect(createdMileages).toMatchInlineSnapshot(`
-        Object {
-          "data": Array [
-            Object {
-              "date": "2021-08-09",
-              "distance": 21.1,
-              "transactionId": "mileageId_a4f91a486455036ff08fac6392d7c73ca4077f18",
-            },
-            Object {
-              "date": "2021-08-10",
-              "distance": 89.1,
-              "transactionId": "mileageId_899077c22a901810574c95009abc072cd3febe1e",
-            },
-          ],
-          "request": Object {
-            "requestId": "requestId_cbac0c1330db2308633bae2f",
-            "timestamp": 1628655978213,
+      const createdMileages: AboundBulkResponse<Mileage> =
+        await abound.mileages.create(TEST_USER_ID, [
+          {
+            distance: 21.1,
+            date: randomDate1,
           },
-        }
+          {
+            distance: 89.1,
+            date: randomDate2,
+          },
+        ]);
+
+      expect(createdMileages.data).toMatchInlineSnapshot(`
+        Array [
+          Object {
+            "date": "${randomDate1}",
+            "description": "On-site Client Visit",
+            "distance": 21.1,
+            "transactionId": "mileageId_test4af7070cfb04a12552a1950e2f0afa660fba",
+          },
+          Object {
+            "date": "${randomDate2}",
+            "description": "On-site Client Visit",
+            "distance": 89.1,
+            "transactionId": "mileageId_test18aab753b0b7fda03a50e609790d088e5c25",
+          },
+        ]
       `);
     });
   });
@@ -64,62 +53,18 @@ describe("Abound Mileages", () => {
     it("returns a promise that resolves to a Mileage on success", async () => {
       const retrievedMileage: AboundResponse<Mileage> =
         await abound.mileages.retrieve(
-          "userId_1dafc40ae9838c2c3e721b2c3c362252ab55eb9f",
-          "mileageId_a4f91a486455036ff08fac6392d7c73ca4077f18"
+          TEST_USER_ID,
+          "mileageId_test4af7070cfb04a12552a1950e2f0afa660fba"
         );
 
-      expect(retrievedMileage).toMatchInlineSnapshot(`
+      expect(retrievedMileage.data).toMatchInlineSnapshot(`
         Object {
-          "data": Object {
-            "date": "2021-08-09",
-            "distance": 21.1,
-            "transactionId": "mileageId_a4f91a486455036ff08fac6392d7c73ca4077f18",
-          },
-          "request": Object {
-            "requestId": "requestId_038d9dcdf0a008ed80742dee",
-            "timestamp": 1628656933874,
-          },
+          "date": "2020-01-09",
+          "description": "On-site Client Visit",
+          "distance": 23.1,
+          "transactionId": "mileageId_test4af7070cfb04a12552a1950e2f0afa660fba",
         }
       `);
     });
   });
-
-  function initMocks() {
-    nock(V2_SANDBOX_URL)
-      .post("/users/userId_1dafc40ae9838c2c3e721b2c3c362252ab55eb9f/mileage")
-      .reply(200, {
-        data: [
-          {
-            distance: 21.1,
-            date: "2021-08-09",
-            transactionId: "mileageId_a4f91a486455036ff08fac6392d7c73ca4077f18",
-          },
-          {
-            distance: 89.1,
-            date: "2021-08-10",
-            transactionId: "mileageId_899077c22a901810574c95009abc072cd3febe1e",
-          },
-        ],
-        request: {
-          timestamp: 1628655978213,
-          requestId: "requestId_cbac0c1330db2308633bae2f",
-        },
-      });
-
-    nock(V2_SANDBOX_URL)
-      .get(
-        "/users/userId_1dafc40ae9838c2c3e721b2c3c362252ab55eb9f/mileage/mileageId_a4f91a486455036ff08fac6392d7c73ca4077f18"
-      )
-      .reply(200, {
-        data: {
-          transactionId: "mileageId_a4f91a486455036ff08fac6392d7c73ca4077f18",
-          distance: 21.1,
-          date: "2021-08-09",
-        },
-        request: {
-          timestamp: 1628656933874,
-          requestId: "requestId_038d9dcdf0a008ed80742dee",
-        },
-      });
-  }
 });
--- tests/resources/TaxCategories.spec.ts
+++ tests/resources/TaxCategories.spec.ts
@@ -1,19 +1,12 @@
-import nock from "nock";
-
 import Abound from "../../src/abound";
 import { AboundResponse } from "../../src/resources/base/AboundResponse";
-import { createAboundClient, V2_SANDBOX_URL } from "../utils";
+import { createAboundClient } from "../utils";
 
 describe("Abound Tax Categories", () => {
   let abound: Abound;
 
   beforeAll(() => {
     abound = createAboundClient();
-    initMocks();
-  });
-
-  afterAll(() => {
-    nock.restore();
   });
 
   describe("retrieve", () => {
@@ -21,47 +14,8 @@ describe("Abound Tax Categories", () => {
       const taxCategories: AboundResponse<string[]> =
         await abound.taxCategories.retrieve(2021);
 
-      expect(taxCategories).toMatchInlineSnapshot(`
-        Object {
-          "data": Array [
-            "Advertising and Marketing",
-            "Car and Truck",
-            "Commission and Fees",
-            "Contract Labor",
-            "Depletion",
-            "Depreciation",
-            "Employee Benefits Program",
-            "Insurance",
-            "Interest (Mortgage)",
-            "Interest (Other)",
-            "Legal and Professional Fees",
-            "Office Expense",
-            "Pension and Profit Sharing",
-            "Rent (Vehicles and Equipment)",
-            "Rent (Business Property)",
-            "Repairs and Maintenance",
-            "Supplies",
-            "Taxes and Licenses",
-            "Travel",
-            "Meals",
-            "Utilities",
-            "Wages",
-            "Other",
-          ],
-          "request": Object {
-            "requestId": "requestId_dfc70284ffd30c02b300e9b0",
-            "timestamp": 1629660582355,
-          },
-        }
-      `);
-    });
-  });
-
-  function initMocks() {
-    nock(V2_SANDBOX_URL)
-      .get("/taxCategories/2021")
-      .reply(200, {
-        data: [
+      expect(taxCategories.data).toMatchInlineSnapshot(`
+        Array [
           "Advertising and Marketing",
           "Car and Truck",
           "Commission and Fees",
@@ -85,11 +39,8 @@ describe("Abound Tax Categories", () => {
           "Utilities",
           "Wages",
           "Other",
-        ],
-        request: {
-          timestamp: 1629660582355,
-          requestId: "requestId_dfc70284ffd30c02b300e9b0",
-        },
-      });
-  }
+        ]
+      `);
+    });
+  });
 });
