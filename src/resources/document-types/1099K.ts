a842740610ba658b93a354225a53d820721bfc21
Mark Mullan <markrmullan@gmail.com>
2022-01-19 21:03:57 -0800

    xo fix

--- src/resources/document-types/1099K.ts
+++ src/resources/document-types/1099K.ts
@@ -8,7 +8,7 @@ export interface Ten99KDocumentRequest extends BaseDocumentRequest {
   year: number;
 
   /**
-   * The type of payer. Payment settlement entity (PSE) or Electronic Payment Facilitator (EPF)/Other 
+   * The type of payer. Payment settlement entity (PSE) or Electronic Payment Facilitator (EPF)/Other
    * third party.
    */
   payerClassification: PayerClassification;
@@ -19,13 +19,13 @@ export interface Ten99KDocumentRequest extends BaseDocumentRequest {
   transactionsReportedClassification: TransactionsReportedClassification;
 
   /**
-   * The payment settlement entity name. Required if `payerClassification` is 
+   * The payment settlement entity name. Required if `payerClassification` is
    * PayerClassification.PAYMENT_SETTLEMENT_ENTITY.
    */
   pseName?: string;
 
   /**
-   * The payment settlement entity phone number. Required if `payerClassification` is 
+   * The payment settlement entity phone number. Required if `payerClassification` is
    * PayerClassification.PAYMENT_SETTLEMENT_ENTITY.
    */
   psePhoneNumber?: string;
@@ -36,34 +36,34 @@ export interface Ten99KDocumentRequest extends BaseDocumentRequest {
   accountNumber?: string;
 
   /**
-   * Aggregate gross amount of payment card/third party network transactions made to you through the PSE 
+   * Aggregate gross amount of payment card/third party network transactions made to you through the PSE
    * during the calendar year.
    */
   aggregateGrossAmount?: number; // float
 
   /**
-   * The aggregate gross amount of all reportable payment transactions made to you through the PSE during the 
-   * calendar year where the card was not present at the time of the transaction or the card number was keyed 
-   * into the terminal. Typically, this relates to online sales, phone sales, or catalogue sales. If 
-   * transactionsReportedClassification is TransactionsReportedClassification.THIRD_PARTY_NETWORK, or if 
+   * The aggregate gross amount of all reportable payment transactions made to you through the PSE during the
+   * calendar year where the card was not present at the time of the transaction or the card number was keyed
+   * into the terminal. Typically, this relates to online sales, phone sales, or catalogue sales. If
+   * transactionsReportedClassification is TransactionsReportedClassification.THIRD_PARTY_NETWORK, or if
    * these are third party network transactions, card not present transactions will not be reported.
    */
   aggregateGrossAmountCardNotPresent?: number; // float
 
   /**
-   * Payment brands use merchant category codes (MCCs) to classify merchants and businesses by the type of 
+   * Payment brands use merchant category codes (MCCs) to classify merchants and businesses by the type of
    * goods or services provided. It is a four (4) digit code.
    */
   merchantCategoryCode?: string;
 
   /**
-   * The number of payment transactions (not including refund transactions) processed through the payment 
+   * The number of payment transactions (not including refund transactions) processed through the payment
    * card/third party network.
    */
   numberOfPaymentTransactions: number;
 
   /**
-   * The federal income tax withheld. Generally, a payer must backup withhold if you did not furnish your 
+   * The federal income tax withheld. Generally, a payer must backup withhold if you did not furnish your
    * TIN or you did not furnish the correct TIN to the payer.
    */
   federalIncomeTaxWithheld?: number; // float
--- src/resources/document-types/1099NEC.ts
+++ src/resources/document-types/1099NEC.ts
@@ -3,35 +3,35 @@ import { StateTaxInfoWithIncome } from "./StateTaxInfo";
 
 // request body
 export interface Ten99NECDocumentRequest extends BaseDocumentRequest {
-    type: DocumentType.TEN99NEC;
-    payerId: string;
-    year: number;
+  type: DocumentType.TEN99NEC;
+  payerId: string;
+  year: number;
 
-    /**
-     * If the payer assigns the contractor a unique number for its records, enter the account number in 
-     * this field. If not, leave the field blank.
-     */
-    accountNumber?: string;
+  /**
+   * If the payer assigns the contractor a unique number for its records, enter the account number in
+   * this field. If not, leave the field blank.
+   */
+  accountNumber?: string;
 
-    /**
-     * The total amount remitted by payer to recipient during the year.
-     */
-    nonemployeeCompensation: number;
+  /**
+   * The total amount remitted by payer to recipient during the year.
+   */
+  nonemployeeCompensation: number;
 
-    /**
-     * If true, consumer products totaling $5,000 or more were sold to recipient for resale, on a buy-sell, 
-     * a deposit-commission, or other basis.
-     */
-    hasDirectSalesOver5000?: boolean;
+  /**
+   * If true, consumer products totaling $5,000 or more were sold to recipient for resale, on a buy-sell,
+   * a deposit-commission, or other basis.
+   */
+  hasDirectSalesOver5000?: boolean;
 
-    /**
-     * The federal income tax withheld. A payer must backup withhold on certain payments if recipient did 
-     * not provide a TIN to the payer.
-     */
-    federalIncomeTaxWithheld?: number;
+  /**
+   * The federal income tax withheld. A payer must backup withhold on certain payments if recipient did
+   * not provide a TIN to the payer.
+   */
+  federalIncomeTaxWithheld?: number;
 
-    /**
-     * Up to two (2) state tax information objects.
-     */
-    stateTaxInfo?: StateTaxInfoWithIncome[];
+  /**
+   * Up to two (2) state tax information objects.
+   */
+  stateTaxInfo?: StateTaxInfoWithIncome[];
 }
