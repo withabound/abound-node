737fa4cf0d952b192f82391c8e1457cbad217011
Mark Mullan <markrmullan@gmail.com>
2022-04-11 23:41:30 -0700

    now make it pass

--- verify_version.sh
+++ verify_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Verify package.json version matches version.ts
-if ! npx genversion --check-only -v src/util/version.ts; then
+if ! npx genversion --check-only -v --es6 --semi --double src/util/version.ts; then
   echo "Run the following command to fix:
 npx genversion --es6 --semi --double src/util/version.ts"
 
