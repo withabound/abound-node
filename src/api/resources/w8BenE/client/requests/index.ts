4423af1e2002793422b7374026eb2a20dbfc5c5d
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-07-23 16:10:07 +0000

    SDK regeneration

---
+++ src/api/resources/w8BenE/client/requests/index.ts
@@ -0,0 +1,2 @@
+export { type W8BenEListRequest } from "./W8BenEListRequest";
+export { type W8BenECreateRequest } from "./W8BenECreateRequest";
--- src/resources/types/pagination.ts
+++
@@ -1,3 +0,0 @@
-export type Pagination = {
-  page?: number;
-};
